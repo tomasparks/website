d8fda759eb646493a5538faa71565de4e026604d
tomasparks <tomasparks@users.noreply.github.com>
2016-09-10 14:23:17 +1000

    updating website source code.

--- _plugins/jekyll-gallery-generator-master/gallery_generator.rb
+++ _plugins/jekyll-gallery-generator-master/gallery_generator.rb
@@ -110,7 +110,10 @@ puts "Entering AlbumPage:initialize()"
                       #  self.data['url'] = File.join(@dir, @name)
 			self.data['files'] = []
                         @base_album_path = site.config['album_dir'] || 'albums'
-			self.data['image'] = @album_metadata['image'] || File.join(@base_album_path, "./blank_folder.png")
+				if File.exists? File.join(@base_album_path,"#{@dir}.png")
+					self.data['image'] = File.join(@base_album_path,"#{@dir}.png")
+				end
+			self.data['image'] = self.data['image'] || File.join(@base_album_path, "./blank_folder.png")
 
 			self.data['albums'] =  []
 			self.data['description'] = @album_metadata['description'] || ""
