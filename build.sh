a17a0b9c629055a7a4231c1c372dd18d0ed6c689
tomasparks <tomasparks@users.noreply.github.com>
2016-10-20 16:21:48 +1000

    updating theme.

--- build.sh
+++ build.sh
@@ -2,20 +2,8 @@ calibredb catalog /home/tom/blog/_data/ebooks.csv --sort-by title_sort --fields=
 
 #bundle update
 
-jekyll build --verbose --trace
-
-cp -r -f ./assets/img/resized/* ./_site/assets/img/resized/
 
 
-git add --all 
-git commit -a -m "updating website source code."
-git push
-
-cd _site
-git add --all 
-git commit -a -m "updating static website."
-git push
-cd ..
 
 rsync -r --progress --prune-empty-dirs  \
  --include "*.html" \
@@ -54,6 +42,24 @@ cd theme
 git add --all 
 git commit -a -m "updating theme."
 git push
+
+cd ..
+
+
+jekyll build --verbose --trace
+
+cp -r -f ./assets/img/resized/* ./_site/assets/img/resized/
+
+
+git add --all 
+git commit -a -m "updating website source code."
+git push
+
+cd _site
+git add --all 
+git commit -a -m "updating static website."
+git push
+
 
 #wget --spider -r --delete-after -P "/tmp/" -e robots=off https://tomasparks.github.io/
 
