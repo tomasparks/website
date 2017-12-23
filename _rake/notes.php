df74170a683dfdc6841415169cf49f57e4bd0ac7
tomasparks <tomasparks@users.noreply.github.com>
2017-12-23 10:05:09 +1000

    updating website source code.

--- _rake/notes.php
+++ _rake/notes.php
@@ -62,9 +62,9 @@ function create_notes($data) {
 					
 					$book = $data['book'];
 					print_r($book);
-					fwrite($mdfile, "book-title: ".$book['title']."\n");
-					fwrite($mdfile, "book-image_url: ".$book['image_url']."\n");
-					fwrite($mdfile, "book-url: ".$book['url']."\n");					
+					fwrite($mdfile, "book-title: |- ".$book['title']."\n");
+					fwrite($mdfile, "book-image_url: |- ".$book['image_url']."\n");
+					fwrite($mdfile, "book-url: |- ".$book['url']."\n");					
 					
 				    //foreach($note['tags'] as $tagkey => $tag_value) {
 				    //fwrite($mdfile, "tags-".$tagkey.": ".$tag_value."\n");
