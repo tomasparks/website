4280f0c17a127cffe56c7eb0b373f8746bce848e
tomasparks <tomasparks@users.noreply.github.com>
2018-02-23 13:15:22 +1000

    updating website source code.

--- _rake/notes.php
+++ _rake/notes.php
@@ -78,9 +78,9 @@ function create_notes($data,$logfile, $WM_recv) {
 				}
 				
 				$md_array['permalink'] ="/notes/".$note['type']."/".$permdate."/".$hash.".html";
-				$md_array['redirect_from'][] = "/sl/n/".$note['type'][0]."/d".date("YmdHis", strtotime($note['date'])).".html";
-				$md_array['redirect_from'][] = "/sl/n/".$note['type'][0]."/h".$hash.".html";
-				$md_array['redirect_from'][] = "/sl/n/".$note['type'][0]."/".num_to_sxg(date("YmdHisu", strtotime($note['date']))).".html";
+				$md_array['redirect_from'][] = "/sl/n/d".date("YmdHis", strtotime($note['date'])).".html";
+				$md_array['redirect_from'][] = "/sl/n//h".$hash.".html";
+				$md_array['redirect_from'][] = "/sl/n/".num_to_sxg(date("YmdHisu", strtotime($note['date']))).".html";
 				
    	//if (isset($note['tags']) && is_array($note['tags'])) {				
 		//		 $tag_array = $note['tags'];
