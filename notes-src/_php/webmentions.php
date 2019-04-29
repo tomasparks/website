dbda507b742f3c944eceda52f3ed6cc1040ed9b3
tomasparks <tomasparks@users.noreply.github.com>
2019-04-30 09:36:49 +1000

    update notes-src section

--- notes-src/_php/webmentions.php
+++ notes-src/_php/webmentions.php
@@ -16,8 +16,41 @@ $json =json_decode($input, true);
 	            //print_r ($value);
 				//$date = new DateTime("@$epoch");
 				$date_split = date_parse($value['wm-received']);
-				$db[$value['wm-property']]
-				[]=$value;
+				
+				$target_path = parse_url($value['wm-target'], PHP_URL_PATH);
+				//echo "".$target_path."\n";
+				$target_path_arr = array_filter(explode("/", $target_path));
+foreach ($target_path_arr as $path) {
+if (strlen($path)>=1) {break;}
+}
+
+switch ($path) {
+case "archive":
+    $path = "main";
+    break;
+}
+				
+				
+				switch ($value['wm-property']) {
+				    case "in-reply-to":
+				        $db[$path]['replied'][]=$value;
+				    break;
+				    case "like-of":
+				    	$db[$path]['liked'][]=$value;
+				    break;
+				    case "mention-of":
+				    	  $db[$path]['mentioned'][]=$value;
+				    break;
+				    case "repost-of":
+				    	  $db[$path]['repost'][]=$value;
+				    break;
+				    case "bookmark-of":
+				    	  $db[$path]['bookmarked'][]=$value;
+				    break;
+				    case "rsvp":
+				    	  $db[$path]['rsvped'][]=$value;
+				    break;
+				    }
 				
 				
 				// h-card section
@@ -28,6 +61,17 @@ $json =json_decode($input, true);
 				    break;
                     }
 	}
+	
+	
+	  yaml_emit_file( "/home/tom/github/website/sources/gobal/_data/cache/nickname.yml", $hcard);
+	  yaml_emit_file( "/home/tom/github/website/sources/gobal/_data/webmention/output.yml", $db);
+	  foreach ($db as $key => $value) {
+	  echo "key: ".$key."\n";
+	    	if(is_array($key))	{
+	                      mkdir("/home/tom/github/website/sources/notes-src/_data/webmention/".$key."/");
+	                      chdir("/home/tom/github/website/sources/notes-src/_data/webmention/".$key."/");
+	  }
+	}  
 //print_r ($db);
-print_r ($hcard);
+//print_r ($db);
 ?>
